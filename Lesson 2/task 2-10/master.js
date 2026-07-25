a920f7fe3d5f0a5c904084295907979c74d8dfb3
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-07-25 19:51:02 +0300

    lesson 2 100% done

---
+++ Lesson 2/task 2-10/master.js
@@ -0,0 +1,12 @@
+let a = +prompt('write number');
+let b = +prompt('write number');
+
+if (a > b) {
+    console.log(a);
+}
+else if (b > a) {
+    console.log(b);
+}
+else {
+    console.log('Same');
+}
---
+++ Lesson 2/task 2-11/master.js
@@ -0,0 +1,5 @@
+let x = false;
+
+x ||= 'default';
+
+console.log(x);
