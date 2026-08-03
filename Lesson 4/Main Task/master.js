07225f5c7dd07af2d8a01d1c22b8c96887263248
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-03 17:11:34 +0300

    Main Task full done 100%

--- Lesson 4/Main Task/master.js
+++ Lesson 4/Main Task/master.js
@@ -2,9 +2,22 @@
 
 // Приклад exchange(10000 *(uan)*,[{currency:'USD'*(валюта)*,value:25*(курс)*},{currency:'EUR',value:42}],'USD') // => 400 *(10000/25 = 400USD)*
 
-
-
-
 function exchange(sumUAH,currencyValues,exchangeCurrency) {
-
+    for (let i = 0; i < currencyValues.length; i++) {
+        if (currencyValues[i].currency === exchangeCurrency) {
+            return sumUAH / currencyValues[i].value;
+        }
+    }
 }
+
+let result = exchange(
+    10000,
+    [
+
+        {currency:'USD',value:25},
+        {currency:'EUR',value:10}
+
+    ],
+    'USD'
+)
+console.log(result);
