a2291291ef17aeb61abdd876045155a21e1d9e78
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-03 03:54:30 +0300

    Main Task start

---
+++ Lesson 4/Main Task/master.js
@@ -0,0 +1,10 @@
+// Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+
+// Приклад exchange(10000 *(uan)*,[{currency:'USD'*(валюта)*,value:25*(курс)*},{currency:'EUR',value:42}],'USD') // => 400 *(10000/25 = 400USD)*
+
+
+
+
+function exchange(sumUAH,currencyValues,exchangeCurrency) {
+
+}
