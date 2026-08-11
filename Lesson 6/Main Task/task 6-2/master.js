8dfa8312ce240bc5e791f3ce97fe9ba0b0af621a
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-12 02:03:34 +0300

    main task 6-2 full done

--- Lesson 6/Main Task/task 6-2/master.js
+++ Lesson 6/Main Task/task 6-2/master.js
@@ -14,3 +14,58 @@
 //
 //     clubs:[]
 // }
+
+
+let suits = ['spade', 'diamond', 'heart', 'clubs'];
+let values = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+
+let cards = [];
+
+for (let suit of suits) {
+    for (let value of values) {
+
+        if (suit === 'spade' || suit === 'clubs') {
+            cards.push({
+                cardSuit: suit,
+                Value: value,
+                Color: 'black'
+            });
+        } else {
+            cards.push({
+                cardSuit: suit,
+                Value: value,
+                Color: 'red'
+            });
+
+        }
+    }
+}
+
+let result = cards.reduce((acc, card) => {
+
+    if (card.cardSuit === 'spade') {
+        acc.spades.push(card);
+    }
+
+    if (card.cardSuit === 'diamond') {
+        acc.diamonds.push(card);
+    }
+
+    if (card.cardSuit === 'heart') {
+        acc.hearts.push(card);
+    }
+
+    if (card.cardSuit === 'clubs') {
+        acc.clubs.push(card);
+    }
+
+    return acc;
+
+}, {
+    spades: [],
+    diamonds: [],
+    hearts: [],
+    clubs: []
+});
+
+console.log(result);
