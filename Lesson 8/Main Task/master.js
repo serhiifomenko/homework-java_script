ba5580c23aed43b59f7e95445d274eb743cbfe48
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-18 23:28:59 +0300

    main task is done

--- Lesson 8/Main Task/master.js
+++ Lesson 8/Main Task/master.js
@@ -5,3 +5,69 @@
 // Додати перевірки на undefined, null, NaN.
 //
 // Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції, які в нього були до цього моменту.
+
+const products = [
+    {
+        name: 'Phone',
+        price: 1200,
+        details: {
+            manufacturer: 'Apple',
+            country: 'USA'
+        }
+    },
+    {
+        name: 'Laptop',
+        price: 2000,
+        details: {
+            manufacturer: 'Lenovo',
+            country: 'China'
+        }
+    }
+];
+
+
+
+function copyProducts(product) {
+    if (
+        product === undefined ||
+        product === null ||
+        Number.isNaN(product)
+    ) {
+        return product;
+    }
+
+    if (typeof product !== 'object') {
+        return product;
+    }
+
+    let copy;
+
+    if (Array.isArray(product)) {
+        copy = [];
+    } else {
+        copy = {};
+    }
+
+    for (const key in product) {
+        if (typeof product[key] === 'function') {
+            copy[key] = product[key];
+        } else {
+            copy[key] = copyProducts(product[key]);
+        }
+    }
+
+    return copy;
+}
+
+console.log(copyProducts(products));
+
+
+
+
+
+
+
+
+
+
+
