6ac5298cd433e64e8d53b729ab013779a266324f
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-20 03:35:13 +0300

    50% secondary tasks is done

--- Lesson 9/Secondary Tasks/task 9-1/master.js
+++ Lesson 9/Secondary Tasks/task 9-1/master.js
@@ -9,3 +9,21 @@
 // – додати цей блок в body.
 //
 // – клонувати його повністю, та додати клон в body.
+
+
+const newBlock = document.createElement("div");
+console.log(newBlock);
+newBlock.classList.add("wrap");
+newBlock.classList.add("collapse");
+newBlock.classList.add("alpha");
+newBlock.classList.add("beta");
+
+newBlock.style.backgroundColor = "#b63232";
+newBlock.style.color = "#72379f";
+newBlock.style.fontSize = "20px";
+
+document.body.appendChild(newBlock);
+
+const cloneBlock = newBlock.cloneNode(true);
+document.body.appendChild(cloneBlock);
+
--- Lesson 9/Secondary Tasks/task 9-2/master.js
+++ Lesson 9/Secondary Tasks/task 9-2/master.js
@@ -5,3 +5,17 @@
 //     [‘Main’,’Products’,’About us’,’Contacts’]
 //
 // Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+
+const menu = ['Main', 'Products', 'About us', 'Contacts'];
+
+const newUl = document.createElement('ul')
+
+menu.forEach((item) => {
+        const newLi = document.createElement('li')
+        newLi.innerText = item;
+        newUl.append(newLi);
+
+})
+console.log(newUl)
+document.body.appendChild(newUl);
