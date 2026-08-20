9e5d14a837dd448367441ffe31fc476039756cc2
Serhii Fomenko <sergeyfomenko622@gmail.com>
2026-08-20 03:51:45 +0300

    secondary tasks full done

--- Lesson 9/Secondary Tasks/task 9-3/master.js
+++ Lesson 9/Secondary Tasks/task 9-3/master.js
@@ -21,3 +21,24 @@
 // Для кожного елементу масиву зробити блок, в якому вивести інформацію про title та monthDuration
 //
 // Завдання робити через цикли.
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+coursesAndDurationArray.forEach((course) => {
+    const newBlock = document.createElement("div");
+    newBlock.innerHTML = course.title + ' - ' + course.monthDuration;
+    document.body.append(newBlock);
+
+    console.log(newBlock);
+
+});
+
+
--- Lesson 9/Secondary Tasks/task 9-4/master.js
+++ Lesson 9/Secondary Tasks/task 9-4/master.js
@@ -4,7 +4,7 @@
 //
 // let coursesAndDurationArray = [
 //
-//     {title: ‘JavaScript Complex’, monthDuration: 5},
+// {title: ‘JavaScript Complex’, monthDuration: 5},
 //
 // {title: ‘Java Complex’, monthDuration: 6},
 //
@@ -23,3 +23,31 @@
 // За допомоги скріпта для кожного елементу масиву зробити <div class=’item’> ,  в якому буде <h1 class=’heading’>  з title  елементу, та <p class=’description’> з monthDuration елементу.
 //
 //     Завдання робити через цикли.
+
+
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+coursesAndDurationArray.forEach(course => {
+    const makeDiv = document.createElement('div');
+    makeDiv.classList.add('item');
+
+    const makeTitle = document.createElement('h1');
+    makeTitle.classList.add('heading');
+    makeTitle.innerText = course.title;
+
+    const makeText = document.createElement('p');
+    makeText.classList.add('description');
+    makeText.innerText = course.monthDuration;
+
+    makeDiv.appendChild(makeTitle);
+    makeDiv.appendChild(makeText);
+
+    document.body.appendChild(makeDiv);
+})
